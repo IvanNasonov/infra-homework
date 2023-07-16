2ceb06b6cea63d51eb834aeee92f4cc02530a034
IvanNasonov <jessica_pes@mail.ru>
2023-07-16 17:31:09 +0600

    ci(release): try to generate changelog

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -2,13 +2,9 @@
 # create release branch for hofixes, create issue with RELEASE tag, create changelog and update version
 
 # GutHub environment variables:
-# $GITHUB_ACCESS_TOKEN
-# $GITHUB_REPOSITORY
-# $GITHUB_USER_EMAIL
-# $GITHUB_USER_NAME
-# $GITHUB_PROJECT_NAME
-# $GITHUB_PROJECT_ID
-# $ISSUE_URL_FORMAT
+# $ACCESS_TOKEN
+# $USER_EMAIL
+# $USER_NAME
 
 # Variables
 # $1 - version tag name
@@ -20,11 +16,14 @@ git config --global user.name $USER_NAME
 
 # create release branch for hotfixes
 NEW_BRANCH_NAME="release-$1"
-
 echo "creating branch $NEW_BRANCH_NAME"
-
 git checkout -b $NEW_BRANCH_NAME
 git push --set-upstream origin $NEW_BRANCH_NAME
 
-#creating a release issue
-gh issue create --title "Release $1" --body "Insert body here" --label "RELEASE" --assignee $2
+
+# generate changelog and change app version using standard-version
+VERSION_NUMBER=${1:1}
+npx standard-version --release-as $VERSION_NUMBER --skip.tag
+
+# creating a release issue
+gh issue create --title "Release $1" --body "Insert body here" --label "RELEASE" --assignee $2 --body-file ./CHANGELOG.md
