0912cf91e3810b42f247accb34623fc18edb0ff8
IvanNasonov <jessica_pes@mail.ru>
2023-07-16 19:36:43 +0600

    ci(release): maybe get correct changelog

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -9,6 +9,7 @@
 # $1 - version tag name
 # $2 - user who initialted release
 
+
 # configure git user
 echo "Setting up git user"
 echo "Setting name to $user_name"
@@ -16,7 +17,15 @@ echo "Setting email to $user_email"
 git config --global user.email $user_name
 git config --global user.name $user_email
 
-echo $(git config --global --list)
+# remember previous version
+PREV_VERSION=$(node -p "require('./package.json').version")
+PREV_TAG=v$PREV_VERSION
+
+# update version and changelog
+VERSION_NUMBER=${1:1}
+echo "Creating changelog for $VERSION_NUMBER"
+npx standard-version --release-as $VERSION_NUMBER --skip.tag
+git push origin master
 
 # create release branch for hotfixes
 NEW_BRANCH_NAME="release-$1"
@@ -24,11 +33,15 @@ echo "Creating branch $NEW_BRANCH_NAME"
 git checkout -b $NEW_BRANCH_NAME
 git push --set-upstream origin $NEW_BRANCH_NAME
 
+# get release notes from diff in changelog
+# 1. get diff of changelog file which will return only new additions since last version
+# 2. egrep prints only lines of diff with pluses in the beginning
+# 3. cut cunts the "+" character and limits line length to 1024 characrers
+# 4. awk prints the lines with newlines
+echo "Preparing release notes"
+RELEASE_NOTES=$(git diff $PREV_TAG $TAG CHANGELOG.md | egrep '^\+' | cut -c2-1024 | awk '{printf "%s\\n", $0}')
+echo $RELEASE_NOTES
 
-# generate changelog and change app version using standard-version
-VERSION_NUMBER=${1:1}
-echo "Creating changelog for $VERSION_NUMBER"
-npx standard-version --release-as $VERSION_NUMBER --skip.tag
 
 # creating a release issue
-gh issue create --title "Release $1" --body "Insert body here" --label "RELEASE" --assignee $2 --body-file ./CHANGELOG.md
+gh issue create --title "Release $1" --body "Insert body here" --label "RELEASE" --assignee $2 --body-file $RELEASE_NOTES
