6617276f4817a02a622c22e671ed03930092c1b0
IvanNasonov <jessica_pes@mail.ru>
2023-07-16 18:19:20 +0600

    fix(ci): release

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -13,8 +13,8 @@
 echo "Setting up git user"
 git config --global user.email $user_name
 git config --global user.name $user_email
-git config --global user.email
-git config --global user.name
+echo git config --global user.email
+echo git config --global user.name
 
 # create release branch for hotfixes
 NEW_BRANCH_NAME="release-$1"
@@ -26,7 +26,7 @@ git push --set-upstream origin $NEW_BRANCH_NAME
 # generate changelog and change app version using standard-version
 VERSION_NUMBER=${1:1}
 echo "creating changelog for $VERSION_NUMBER"
-npx standard-version --release-as "$VERSION_NUMBER" --skip.tag
+npx standard-version -- --release-as "$VERSION_NUMBER" --skip.tag
 
 # creating a release issue
 gh issue create --title "Release $1" --body "Insert body here" --label "RELEASE" --assignee $2 --body-file ./CHANGELOG.md
